6783064192c0aadba3073825853c071f679fe25c
MEZ <mez@bitwormhole.com>
2023-10-20 21:09:26 +0800

    update to v1.0.7

--- module_t.go
+++ module_t.go
@@ -9,8 +9,8 @@ import (
 
 const (
 	theModuleName      = "github.com/starter-go/libgin"
-	theModuleVersion   = "v1.0.6"
-	theModuleRevision  = 7
+	theModuleVersion   = "v1.0.7"
+	theModuleRevision  = 8
 	theModuleResPath   = "src/main/resources"
 	theModuleResPathDT = "src/devtools/resources"
 )
