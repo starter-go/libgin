83ef5be6fdeefecb1d25ab53e259cb1cc383616e
MEZ <mez@bitwormhole.com>
2023-10-20 21:07:36 +0800

    修改为：基于 context2 实现的 libgin.ContextHolder

--- context_holder.go
+++ context_holder.go
@@ -5,9 +5,12 @@ import (
 	"fmt"
 
 	"github.com/gin-gonic/gin"
+	"github.com/starter-go/base/context2"
 )
 
 const theContextHolderKey = "libgin.ContextHolder#binding"
+
+////////////////////////////////////////////////////////////////////////////////
 
 // ContextHolder 用来绑定 gin.Context & context.Context
 type ContextHolder struct {
@@ -19,13 +22,41 @@ func (inst *ContextHolder) GinContext() *gin.Context {
 	return inst.gc
 }
 
+func (inst *ContextHolder) init() context2.Values {
+	const key = theContextHolderKey
+	context2.Setup(inst)
+	inst.SetValue(key, inst)
+	return inst
+}
+
+// GetValue ...
+func (inst *ContextHolder) GetValue(key any) any {
+	return inst.gc.Value(key)
+}
+
+// SetValue ...
+func (inst *ContextHolder) SetValue(key, value any) {
+	skey := key.(string)
+	inst.gc.Set(skey, value)
+}
+
+// Context ...
+func (inst *ContextHolder) Context() context.Context {
+	return inst.gc
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
 // GetContextHolder 取 ContextHolder
 func GetContextHolder(cc context.Context) (*ContextHolder, error) {
 	const key = theContextHolderKey
-	o := cc.Value(key)
-	h, ok := o.(*ContextHolder)
+	values, err := context2.GetValues(cc)
+	if err != nil {
+		return nil, err
+	}
+	h, ok := values.GetValue(key).(*ContextHolder)
 	if !ok {
-		return nil, fmt.Errorf("no libgin.ContextHolder binding")
+		return nil, fmt.Errorf("need invoke libgin.BindContext() first")
 	}
 	return h, nil
 }
@@ -33,12 +64,17 @@ func GetContextHolder(cc context.Context) (*ContextHolder, error) {
 // BindContext 绑定 gin.Context & context.Context
 func BindContext(c *gin.Context) *ContextHolder {
 	const key = theContextHolderKey
-	o := c.Value(key)
-	h, ok := o.(*ContextHolder)
-	if ok {
-		return h
+	values, err := context2.GetValues(c)
+	if values == nil || err != nil {
+		h2 := &ContextHolder{gc: c}
+		h2.init()
+		values = h2
 	}
-	h = &ContextHolder{gc: c}
-	c.Set(key, h)
-	return h
+	holder, ok := values.GetValue(key).(*ContextHolder)
+	if !ok {
+		panic("bad binding of libgin.ContextHolder")
+	}
+	return holder
 }
+
+////////////////////////////////////////////////////////////////////////////////
--- go.mod
+++ go.mod
@@ -21,7 +21,7 @@ require (
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
 	github.com/starter-go/afs v0.9.2 // indirect
 	github.com/starter-go/application v0.9.13 // indirect
-	github.com/starter-go/base v0.9.8 // indirect
+	github.com/starter-go/base v0.9.9 // indirect
 	github.com/starter-go/configen v0.9.3 // indirect
 	github.com/starter-go/starter v1.0.3 // indirect
 	github.com/starter-go/vlog v0.9.5 // indirect
